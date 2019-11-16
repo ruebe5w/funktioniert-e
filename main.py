a4a0a24e0131dac056577c78417be8f8a7c9b4e8
ruebe5w <ruebefuenfw@gmail.com>
2019-11-16 11:08:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import time
+
+headers = {'Content-Type':'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer 09f116f7-7e97-4fe6-b006-731689e57d9e'}
+root = 'https://www.intern-e.evlka.de/toro/api/v1/'
+
+data = {'message':'Diese Nachricht wurde über Python an Intern-E verschickt.'}
+id = 'CONVERSATION%2C21956'
+command1 = 'conversation/'
+command2 = '/message'
+
+
+def post(data, id, command1, command2):
+    command = command1 + id + command2
+    url = root + command
+    resp = requests.post(url, json.dumps(data), None, headers=headers)
+    if resp.status_code != 200:
+        # This means something went wrong.
+        print(resp.status_code)
+    print(resp.json())
+
+
+start = time.time()
+post(data, id, command1, command2)
+ende = time.time()
+print('{:5.3f}s'.format(ende - start))
+data={'message':'Diese Nachricht hat '+'{:5.3f}s'.format(ende - start)+' zum Senden gebraucht.'}
+post(data,id,command1,command2)
+# curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Authorization: Bearer 09f116f7-7e97-4fe6-b006-731689e57d9e' -d '{"message":"Diese Nachricht wurde über die Intern-E-API gesendet."}' 'https://www.intern-e.evlka.de/toro/api/v1/conversation/CONVERSATION%2C21956/message'
